4274ea8c54a8d887990f1eca5e24bf62a68960cb
Taabeer Murtaza <taabeer@alpha1.pc>
2021-05-30 12:21:36 +0500

    bypassed csrf token (was causing problems)

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import UploadForm
 from .models import File
 
 # Create your views here.
 
+# Bypassing CSRF Token
+@csrf_exempt
 def index(request):
     form = UploadForm(request.POST or None, request.FILES or None)
     files = File.objects.order_by('-pk')
